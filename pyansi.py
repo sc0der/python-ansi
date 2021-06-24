158f4722074a2221c56f342f28e0c45824f6900c
sc0der <siriusproger@gmail.com>
2021-06-25 03:42:53 +0500

    init commit

---
+++ pyansi.py
@@ -0,0 +1,32 @@
+import os
+
+os.system("cls") #use this for windows. change to os.system("clear") for linux
+
+COLORS = {\
+"black":"\u001b[30;1m",
+"red": "\u001b[31;1m",
+"green":"\u001b[32m",
+"yellow":"\u001b[33;1m",
+"blue":"\u001b[34;1m",
+"magenta":"\u001b[35m",
+"cyan": "\u001b[36m",
+"white":"\u001b[37m",
+"yellow-background":"\u001b[43m",
+"black-background":"\u001b[40m",
+"cyan-background":"\u001b[46;1m",
+}
+#You can add more colors and backgrounds to the dictionary if you like.
+
+
+def colorText(text):
+    for color in COLORS:
+        text = text.replace("[[" + color + "]]", COLORS[color])
+    return text
+
+
+#Example printing out some text
+
+#Example printing out an ASCII file
+f  = open("test.txt","r")
+ascii = "".join(f.readlines())
+print(colorText(ascii)) 
