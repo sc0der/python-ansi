f1b0caa1e7c54da6e33476535afe34cb0ffbfbe6
Shekhov Ismoil <veyxov@gmail.com>
2023-08-21 15:14:13 +0500

    feat: cross-platform terminal claring
    also added main function

--- pyansi.py
+++ pyansi.py
@@ -1,32 +1,47 @@
 import os
 
-os.system("cls") #use this for windows. change to os.system("clear") for linux
+import os
+import subprocess
 
-COLORS = {\
-"black":"\u001b[30;1m",
-"red": "\u001b[31;1m",
-"green":"\u001b[32m",
-"yellow":"\u001b[33;1m",
-"blue":"\u001b[34;1m",
-"magenta":"\u001b[35m",
-"cyan": "\u001b[36m",
-"white":"\u001b[37m",
-"yellow-background":"\u001b[43m",
-"black-background":"\u001b[40m",
-"cyan-background":"\u001b[46;1m",
-}
-#You can add more colors and backgrounds to the dictionary if you like.
+def clear_terminal():
+    if os.name == 'posix':  # Linux/Unix/Mac
+        subprocess.call('clear', shell=True)
+    elif os.name == 'nt':   # Windows
+        subprocess.call('cls', shell=True)
+    else:
+        raise Exception("Cannot clear terminal. Unsupported operating system.")
+
+def main():
+    clear_terminal()
+
+    COLORS = {\
+    "black":"\u001b[30;1m",
+    "red": "\u001b[31;1m",
+    "green":"\u001b[32m",
+    "yellow":"\u001b[33;1m",
+    "blue":"\u001b[34;1m",
+    "magenta":"\u001b[35m",
+    "cyan": "\u001b[36m",
+    "white":"\u001b[37m",
+    "yellow-background":"\u001b[43m",
+    "black-background":"\u001b[40m",
+    "cyan-background":"\u001b[46;1m",
+    }
+    #You can add more colors and backgrounds to the dictionary if you like.
 
 
-def colorText(text):
-    for color in COLORS:
-        text = text.replace("[[" + color + "]]", COLORS[color])
-    return text
+    def colorText(text):
+        for color in COLORS:
+            text = text.replace("[[" + color + "]]", COLORS[color])
+        return text
 
 
-#Example printing out some text
+    #Example printing out some text
 
-#Example printing out an ASCII file
-f  = open("test.txt","r")
-ascii = "".join(f.readlines())
-print(colorText(ascii)) 
+    #Example printing out an ASCII file
+    f  = open("test.txt","r")
+    ascii = "".join(f.readlines())
+    print(colorText(ascii)) 
+
+if __name__ == "__main__":
+    main()
